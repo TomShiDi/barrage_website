6f1f44d7bbe272e387731aec549300d073c743cb
TomShiDi <1341109792@qq.com>
2020-07-27 23:38:21 +0800

    名言警句模块开发

---
+++ barrage-spring/src/main/java/barrage/demo/controller/QuotesController.java
@@ -0,0 +1,15 @@
+package barrage.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author TomShiDi
+ * @Since 2020/7/27
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class QuotesController {
+
+}
